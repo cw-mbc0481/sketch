ed2253ba6ef6db3082d62b56537739860b3dbb00
cw-mbc0481 <mbc0481@coway.com>
2022-08-11 16:40:37 +0900

    feat(feature/1) 액션 트리거

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  console.log("mode", import.meta.env.VITE_ENV);
-
   return (
     <div className="App">
-      <h1>Github action 테스트입니다.</h1>
+      <h1>Github action {import.meta.env.VITE_ENV} 테스트입니다.</h1>
     </div>
   );
 }
