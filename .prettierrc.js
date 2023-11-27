8815ad320a69d4211c1a25c1a4cd77d11bee41dd
cw-mbc0481 <mbc0481@coway.com>
2023-11-27 15:50:54 +0900

    feat(git-hook-test): 린트 및 프리티어 적용 사항 반영

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 200,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  endOfLine: 'auto',
+};
